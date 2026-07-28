66fdbdcebe58ae8bdc03f08e695ab5614c7b1f1c
Prabhav Gupta <prabhavgupta05@gmail.com>
2026-07-28 21:33:34 +0530

    Added Rest Client Snippet

---
+++ src/main/java/FlagAssessment/Prabhav/client/FlagClient.java
@@ -0,0 +1,30 @@
+package FlagAssessment.Prabhav.client;
+
+import FlagAssessment.Prabhav.DTO.EvaluationResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class FlagClient {
+
+    private final RestClient restClient =
+            RestClient.create("http://localhost:8080");
+
+    public boolean isFeatureEnabled(String tenantId,
+                                    String flag,
+                                    String user) {
+
+        EvaluationResponse response = restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/flags/eval")
+                        .queryParam("flag", flag)
+                        .queryParam("user", user)
+                        .build())
+                .header("X-Tenant-ID", tenantId)
+                .retrieve()
+                .body(EvaluationResponse.class);
+
+        return response != null && response.isEnabled();
+    }
+
+}
